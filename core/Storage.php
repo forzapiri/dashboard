246cc949ad2473c69c25dc114841c90d15c66ebe
Christopher Troup <chris@norex.ca>
2008-12-12 18:53:52 -0500

    beginnings of a file based cacheing backend

---
+++ core/Storage.php
@@ -0,0 +1,74 @@
+<?php
+interface StorageInterface {
+	public function type();
+	public function render();
+}
+
+class Storage extends DBRow implements StorageInterface {
+	function createTable() {
+		$cols = array(
+			'id?',
+			DBColumn::make('text', 'type', 'Type'),
+			DBColumn::make('text', 'original', 'Original File Location'),
+			DBColumn::make('text', 'filename', 'Served File Name'),
+			DBColumn::make('text', 'content_type', 'Content Type'),
+			);
+		return new DBTable("storage", __CLASS__, $cols);
+	}
+	
+	public function type() {
+		return 'blob';
+	}
+	
+	public function shouldCompress() {
+		return false;
+	}
+	
+	public function cachefile() {
+		return SITE_ROOT . '/storage/cache/' . $this->type() . '/' . $this->get('filename') . '-' . $this->get('id') . '.gz';
+	}
+	
+	public function isCached() {
+		return file_exists($this->cachefile());
+	}
+	
+	public function render() {
+		ini_set("zlib.output_compression", "Off");
+		
+		if(!$this->shouldCompress() || (!isset($_SERVER['HTTP_ACCEPT_ENCODING']) or strrpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') === false)) {
+			$compress_file = false;
+		}else{
+			$compress_file = true;
+			$enc = in_array('x-gzip', explode(',', strtolower(str_replace(' ', '', $_SERVER['HTTP_ACCEPT_ENCODING'])))) ? "x-gzip" : "gzip";
+		}
+		
+		if (file_exists(SITE_ROOT . '/' . $this->get('original'))) {
+		    header('Content-Description: File Transfer');
+		    header('Content-Type: ' . $this->get('content_type'));
+		    header('Content-Disposition: filename='.basename($this->get('filename')));
+		    header('Content-Length: ' . filesize(SITE_ROOT . '/' . $this->get('original')));
+		    
+		    if ($compress_file) {
+		    	header("Content-Encoding: " . $enc);
+		    	
+		    	if ($this->isCached()) {
+		    		readfile($this->cachefile());
+		    		exit;
+		    	}
+		    	
+		    	$cacheData = gzencode(file_get_contents(SITE_ROOT . '/' . $this->get('original')), 9);
+		    	echo $cacheData;
+		    	$fp = fopen($this->cachefile(), "w");
+				fwrite($fp, $cacheData);
+				fclose($fp);
+		    } else {
+		    	ob_clean();
+		   	 	flush();
+			    readfile(SITE_ROOT . '/' . $this->get('original'));
+		    }
+		    exit;
+		}
+	}
+}
+DBRow::init('Storage');
+?>
---
+++ core/TextFile.php
@@ -0,0 +1,14 @@
+<?php
+
+class TextFile extends Storage {
+	public function type() {
+		return 'text';
+	}
+	
+	public function shouldCompress() {
+		return true;
+	}
+}
+DBRow::init('TextFile');
+
+?>
