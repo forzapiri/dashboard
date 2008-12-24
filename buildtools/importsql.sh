3fdcc9a079af31cd67b40b13e280e56995f68307
Christopher Troup <chris@norex.ca>
2008-12-24 15:28:31 -0500

    import script should just do initial setup without bugging us for details

--- buildtools/importsql.sh
+++ buildtools/importsql.sh
@@ -33,27 +33,25 @@ stty $stty_orig
 echo Database
 read mysqlDb
 
-echo Import [c]ore or [m]odule sql files?
-read importType
-
 declare -a files
 
-if [ "$importType" = "m" ]
-then
-	#declare -a files
-	i=0
-	dirs=$(find '../modules/' -type d)
-	for dir in $dirs; do
-		tmp=$(find $dir -type f -iname 'schema.sql')
-		if [ "$tmp" != "" ]
-		then
-			files[$i]=$tmp
-			i=`expr $i + 1`
-		fi
-	done
-else
-	files=$(find 'sql/' -type f -iname '*.sql')
-fi
+files=$(find 'sql/' -type f -iname '*.sql')
+for file in $files; do
+	mysql -u $mysqlUser -p$mysqlPass $mysqlDb < $file
+	echo $file imported to $mysqlDb
+done
+
+i=0
+dirs=$(find '../modules/' -type d)
+for dir in $dirs; do
+	tmp=$(find $dir -type f -iname 'schema.sql')
+	if [ "$tmp" != "" ]
+	then
+		files[$i]=$tmp
+		i=`expr $i + 1`
+	fi
+done
+	
 for file in $files; do
 	mysql -u $mysqlUser -p$mysqlPass $mysqlDb < $file
 	echo $file imported to $mysqlDb
