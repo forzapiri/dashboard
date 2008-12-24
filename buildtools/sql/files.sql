5b61a1c2c70164479ef7b3c60265cc893ad2f253
David Wolfe <wolfe@norex.ca>
2008-12-25 03:54:39 +0800

    Added call to getAddEditFormSaveHook() to DBRow Added File.php class which saves uploaded files to filesystem 	    (note buildtools/sql/files.sql) Added image-resizer.php (with minor edits)
    Signed-off-by: Christopher Troup <chris@norex.ca>

---
+++ buildtools/sql/files.sql
@@ -0,0 +1,30 @@
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: letsrent2
+-- ------------------------------------------------------
+-- Server version	5.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `filename` varchar(255) default NULL,
+  `type` varchar(255) default NULL,
+  `description` text,
+  `permission` enum('public','private') default 'public',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
