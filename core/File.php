057366346dd7ab813240c34da065142757c7de8e
David Wolfe <wolfe@norex.ca>
2008-12-24 17:10:05 -0400

    Enriched the getLink options for image files

--- core/File.php
+++ core/File.php
@@ -16,16 +16,68 @@ class File extends DBRow {
 	static function getAll($where = null) {return self::$tables[__CLASS__]->getAllRows($where);}
 
 	function getLink() {return "/" . DATA_STORAGE_DIR . $this->getPath() . $this->getLocalFilename();}
-	function getDirectoryFile() {return $this->getDirectory() . $this->getFilename();}
+	function getImgTag($arg = array(), $alt = null) {
+		$link = $this->getImageLink($arg);
+		if (!$alt) $alt = htmlspecialchars($this->getDescription(), ENT_QUOTES);
+		if (!$alt) $alt = htmlspecialchars($this->getFilename());
+		return "<img src='$link' alt='$alt'/>";
+	}
+	function getImageLink($args = array()) {
+		// $arg is either array(width => 100, height => 200, ...) OR
+		// "width=100&height=100&..."
+		//
+		// Parameters need to be passed in through the URL's query string:
+		// image		absolute path of local image starting with "/" (e.g. /images/toast.jpg)
+		// w or width	maximum width of final image in pixels (e.g. 700)
+		// h or height	maximum height of final image in pixels (e.g. 700)
+		// color		(optional) background hex color for filling transparent PNGs (e.g. 900 or 16a942)
+		// cropratio	(optional) ratio of width to height to crop final image (e.g. 1:1 or 3:2)
+		// nocache		(optional) does not read image from the cache
+		// quality		(optional, 0-100, default: 90) quality of output image
+		$path = $this->getLink();
+		$image_args = array ("w", "width", "h", "height", "color", "cropratio", "quality");
+		$image_flags = array ("nocache");
+		if (is_string ($args)) {
+			$items = split('&', $args);
+			$args = array();
+			foreach ($items as $item) {
+				$tmp = split('=', $item);
+				switch (count($tmp)) {
+				case 1: $args[] = $tmp[0]; break;
+				case 2: $args[$tmp[0]] = $tmp[1]; break;
+				default: error_log ("Too many equal signs");
+				}
+			}
+		}
+		
+		$results = array();
+		
+		foreach ($args as $key => $value) {
+			if (is_numeric ($key) && in_array ($value, $image_flags)) {
+				$results[] = "$key";
+			} elseif (in_array ($key, $image_args)) {
+				$results[] = "$key=$value";
+			} else {
+				error_log ("Key $key=$value not recognized in getLink");
+			}
+		}
+		$args = implode ("&amp;", $results);
+		if ($args) $args = "?$args";
+		return "/im$path$args";
+	}
+	
 	function getSize() {return filesize($this->getDirectoryFile());}
-	function getDirectory() {return SITE_ROOT . "/" . DATA_STORAGE_DIR . $this->getPath();}
-	function getPath() {
+
+	/* Perhaps some of these should be private */
+	private function getDirectoryFile() {return $this->getDirectory() . $this->getFilename();}
+	private function getDirectory() {return SITE_ROOT . "/" . DATA_STORAGE_DIR . $this->getPath();}
+	private function getPath() {
 		$id = (string) $this->getId();
 		if (1&strlen($id)) $id = "0$id";
 		$path = implode('/', str_split($id,2));
 		return "$path/";
 	}
-	function getLocalFilename() {
+	private function getLocalFilename() {
 		// Replaces the extension with one matching the mime type, if available
 		$filename = $this->getFilename();
 		if (!$filename) return "file.tmp";
