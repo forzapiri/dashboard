beb6d8c0b5e6e1601f2db8f8c5773768ed9d1139
David Wolfe <wolfe@norex.ca>
2009-01-06 17:11:23 -0400

    Created new Smarty block {insert_in file="db:site.tpl"}
    Optional argument at, default at='{module class=$module}', is a string
    to be replaced with the processed block content.  The string must
    appear exactly once or a resource not found is generated.

---
+++ core/plugins/block.insert_in.php
@@ -0,0 +1,14 @@
+<?php
+function smarty_block_insert_in ($params, $content, &$smarty) {
+	if (isset ($content)) {
+		$file = $params['file'];
+		$at = @$params['at'];
+		$at = isset($params['at']) ? $params['at'] : '{module class=$module}';
+		$args = serialize(array ('before', $at, $file));
+		$before = $smarty->fetch ("before_after:$args");
+		$args = serialize(array ('after', $at, $file));
+		$after = $smarty->fetch ("before_after:$args");
+		return "$before$content$after";
+	}
+}
+?>
