130ef41e49e012c215d4c09255a8d5aa74b69884
David Wolfe <wolfe@norex.ca>
2009-01-07 01:53:30 +0800

    Adding "text" resource to Smarty.  Syntax:  $smarty->fetch('text:Hello {$foo}!')
    Signed-off-by: Christopher Troup <chris@norex.ca>

---
+++ core/plugins/resource.text.php
@@ -0,0 +1,19 @@
+<?php
+  // $smarty->assign('foo', 'world');
+  // echo $smarty->fetch('text:Hello {$foo}!'); // Hello world!
+function smarty_resource_text_source($tpl_name, &$tpl_source, &$smarty_obj) {
+    $tpl_source = $tpl_name;
+    return true;
+}
+
+function smarty_resource_text_timestamp($tpl_name, &$tpl_timestamp, &$smarty_obj) {
+    $tpl_timestamp = time();
+    return true;
+}
+
+function smarty_resource_text_secure($tpl_name, &$smarty_obj) {
+    return true;
+}
+
+function smarty_resource_text_trusted($tpl_name, &$smarty_obj) {}
+?>
