5e2bdcf7822569005d5b81915706fa5adeb55f03
Adam <adam@Thurloat.local>
2009-01-30 02:40:09 +0800

    Revert "wrap tinymce content in an extra div"
    This reverts commit 62f7940e654e286c6f8024afa50b0d5b01e912f8.
    
    Signed-off-by: Christopher Troup <chris@norex.ca>

--- core/DBColumns.php
+++ core/DBColumns.php
@@ -183,11 +183,6 @@ class DBColumnTinyMCE extends DBColumnsLongText {
 		return $el;
 	}
 	function suggestedMysql() {return "text";}
-	
-	function fromDB($obj) {
-		return '<div class="wysiwyg">' . $obj . '</div>';
-	}
-	
 }
 
 class DBColumnSelect extends DBColumnText {
