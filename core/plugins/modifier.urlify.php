7795c539cf86545610646e7baf40109d77e3a314
David Wolfe <wolfe@norex.ca>
2009-02-01 09:26:48 -0400

    Deleted inappropriate comments

--- core/plugins/modifier.urlify.php
+++ core/plugins/modifier.urlify.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * Smarty plugin to build the javascript for an AJAX call.
- * @file function.helpitem.php
- * @package Smarty
- * @subpackage plugins
- */
-
-/* This is used in place of &nbsp; so as to cover up a bug in our templater editor.
- * 
- * @package Smarty
- * @subpackage plugins
- */
 function smarty_modifier_urlify($str) {
 	$str = htmlentities($str);
 	$str = preg_replace("/(&)([a-z])([a-z]+;)/i", '$2', $str);
