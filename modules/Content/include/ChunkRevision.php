a7f9c26eecbaf8e0e7016abeae2394bc949eac54
David Wolfe <wolfe@norex.ca>
2009-02-02 22:09:11 -0400

    Switched to prepared statements

--- modules/Content/include/ChunkRevision.php
+++ modules/Content/include/ChunkRevision.php
@@ -4,13 +4,13 @@ class ChunkRevision extends DBRow {
 	static function getAll($where = null) {return self::$tables[__CLASS__]->getAllRows($where);}
 	static function make($id = null) {return parent::make($id, __CLASS__);}
 
+	private static $q1;
 	static function getAllContentFor($obj, $status = 'active') {
+		if (!self::$q1) self::$q1 = new Query ("select type,content from chunk c,chunk_revision r where r.status=? and r.parent = c.id and c.parent=? and parent_class=? order by sort", 'sis');
 		$class = get_class($obj);
 		$id = $obj->getId();
 		if (!$id) return array();
-		// Tried using prepared statement.  For some reason didn't work.
-		$sql = "select type,content from chunk c,chunk_revision r where r.status='$status' and r.parent = c.id and c.parent=$id and parent_class='$class' order by sort";
-		$results = Database::singleton()->query_fetch_all($sql);
+		$results = self::$q1->fetchAll($status, $id, $class);
 		$contents = array();
 		foreach ($results as $result) {
 			$contents[] = DBRow::fromDB($result['type'], $result['content']);
@@ -26,17 +26,17 @@ class ChunkRevision extends DBRow {
 		return $result;
 	}
 
+	private static $q2 = null;
 	static function getChunkFormField ($class, $parent, $status = 'active') {
-		$sql = "select type,content from chunk c,chunk_revision r where r.status='$status' and c.parent_class=? and c.parent=?";
-		$query = new Query ($sql, 'si');
-		$result = $query->fetch ($class, $parent);
+		if (!self::$q2) self::$q2 = new Query ("select type,content from chunk c,chunk_revision r where r.status='$status' and c.parent_class=? and c.parent=?",'si');
+		$result = self::$q2->fetch ($class, $parent);
 		return self::getRevisionFormField($result);
 	}
 
+	private static $q3 = null;
 	static function getNamedChunkFormField ($role, $name, $status = 'active') {
-		$sql = "select type,content from chunk c,chunk_revision r where r.status='$status' and role=? and c.parent is null and name=?";
-		$query = new Query ($sql, 'ss');
-		$result = $query->fetch ($role, $name);
+		if (!self::$q3) self::$q3 =  new Query ("select type,content from chunk c,chunk_revision r where r.status='$status' and role=? and c.parent is null and name=?", 'ss');
+		$result = self::$q3->fetch ($role, $name);
 		return self::getRevisionFormField($result);
 	}
 }
