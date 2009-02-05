17030fc0fbf1f1ae11327cb521c1c235aad27173
Christopher Troup <chris@norex.ca>
2009-02-05 10:10:36 -0500

    upgrade phpunit to 3.3.13

--- core/PEAR/PHPUnit/Extensions/Database/DB/ResultSetTable.php
+++ core/PEAR/PHPUnit/Extensions/Database/DB/ResultSetTable.php
@@ -60,7 +60,7 @@ require_once 'PHPUnit/Extensions/Database/DataSet/AbstractTable.php';
  * @author     Mike Lively <m@digitalsandwich.com>
  * @copyright  2009 Mike Lively <m@digitalsandwich.com>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version    Release: 3.3.12
+ * @version    Release: 3.3.13
  * @link       http://www.phpunit.de/
  * @deprecated The PHPUnit_Extension_Database_DataSet_QueryTable should be used instead
  * @see        PHPUnit_Extension_Database_DataSet_QueryTable
