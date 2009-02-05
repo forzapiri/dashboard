774dd83ff123c129c818d8d317ca19c93494567d
David Wolfe <wolfe@norex.ca>
2009-02-04 20:26:13 -0400

    Added check for active page before loading

--- modules/Content/Content.php
+++ modules/Content/Content.php
@@ -13,6 +13,10 @@ class Module_Content extends Module implements linkable {
 		$pageid = ContentPage::keytoid($_REQUEST['page']);
 		$pageid = $pageid['id'];
 		$page = ContentPage::make($pageid);
+		var_log ($page->getStatus());
+		if (!$page->getStatus()) {
+			return $this->smarty->dispErr('404', &$this);
+		}
 		$this->smarty->assign('content',$page);
 		$this->parentSmarty->templateOverride = $page->getSmartyResource();
 		$this->setPageTitle("PAGE TITLE STUB");
