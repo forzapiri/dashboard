79ab7f9964fbc268b8713e327cacd4b6da0a9095
Christopher Troup <chris@norex.ca>
2009-02-11 15:19:25 -0500

    fix apache user checks

--- buildtools/install/install.php
+++ buildtools/install/install.php
@@ -26,9 +26,10 @@ switch ($step) {
 		$content = $s->fetch('step0.htpl');
 		break;
 	case 1:
+		@mkdir(SITE_ROOT . '/files');
 		$fileOwn = posix_getpwuid(fileowner(SITE_ROOT . '/files'));
 		$checks = array(
-			'files directory is owned by the webserver user' => ($_ENV['APACHE_RUN_USER'] == $fileOwn['name']),
+			'files directory is owned by the webserver user' => (exec('whoami') == $fileOwn['name']),
 			'templates directory is readable' => is_readable(SITE_ROOT . '/templates') && is_dir(SITE_ROOT . '/templates'),
 			'cache/templates directory is a read/write directory' => isReadWriteDir(SITE_ROOT . '/cache/templates'),
 			'js/cache soft links to cache/js' => is_link(SITE_ROOT . '/js/cache') && readlink(SITE_ROOT . '/js/cache') === '../cache/js',
