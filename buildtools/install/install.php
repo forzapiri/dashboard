15b641693ae2a17f2c60bf4e67608808fe2964f7
Jacob Godin <jacobgodin@gmail.com>
2009-02-11 13:47:26 -0400

    Tweaked a few things on the installer

--- buildtools/install/install.php
+++ buildtools/install/install.php
@@ -33,7 +33,8 @@ switch ($step) {
 			'js/cache soft links to cache/js' => is_link(SITE_ROOT . '/js/cache') && readlink(SITE_ROOT . '/js/cache') === '../cache/js',
 			'cache/js directory is a read/write directory' => isReadWriteDir(SITE_ROOT . '/cache/js'),
 			'cache/images directory is a read/write directory' => isReadWriteDir(SITE_ROOT . '/cache/images'),
-			'cache/pages directory is a read/write directory' => isReadWriteDir(SITE_ROOT . '/cache/pages')
+			'cache/pages directory is a read/write directory' => isReadWriteDir(SITE_ROOT . '/cache/pages'),
+			'include directory is a read/write directory' => isReadWriteDir(SITE_ROOT . '/include')
 		);
 		$s->assign('checks', $checks);
 		$content = $s->fetch('step1.htpl');
@@ -50,7 +51,7 @@ switch ($step) {
 			'Database Host' => "<input type='text' name='db_host' value='$dbhost' />",
 			'Database Name' => "<input type='text' name='db_name' value='$dbase' />",
 			'Database Username' => "<input type='text' name='db_user' value='$dbuser' />",
-			'Database Password' => "<input type='text' name='db_pass' value='' />",
+			'Database Password' => "<input type='password' name='db_pass' value='$dbpass' />",
 		);
 		$s->assign('fields', $fields);
 		$content = $s->fetch('step2.htpl');
@@ -64,19 +65,19 @@ switch ($step) {
 			if ($_REQUEST['db_name']) $dbase = $_REQUEST['db_name'];
 			if ($_REQUEST['db_pass']) $dbpass = $_REQUEST['db_pass'];
 		}
-		$link = @mysqli_connect($dbhost, $dbuser, $dbpass);
+		$link = @mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 		
-		if ($link && !mysqli_query($link, 'use ' . $dbase)) {
+		/*if ($link && !mysqli_query($link, 'use ' . $dbase)) {
 			mysqli_query($link, 'create database ' . $dbase);
 			$db_exists = false;
 		} else {
 			$db_exists = true;
 		}
-		$s->assign('db_exists', $db_exists);
+		$s->assign('db_exists', $db_exists);*/
 		
 		$checks = array(
 			'Can connect to MySQL Database' => !!$link,
-			'DB has data' => $db_exists
+			//'DB has data' => $db_exists
 		);
 		if ($link) {
 			$db_config = fopen(DB_CONFIG, 'w');
