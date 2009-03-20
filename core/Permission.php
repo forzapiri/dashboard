ed2ac9211a11c268e0970e63366d84bcb18fa739
David Wolfe <wolfe@norex.ca>
2009-03-20 15:06:39 -0300

    Got rid of norex() override of permissions; too many side-effects

--- core/Permission.php
+++ core/Permission.php
@@ -28,17 +28,8 @@ class Permission extends DBRow {
 
 	static private $warned = false;
 	static function hasPerm($group, $class, $key) {
-		$perm = self::getAll('where group_id=? and class=? and `key`=? and status=1', 'iss',
-							 $group, $class, $key);
-		if ($perm) {
-			return true;
-		} else if (SiteConfig::norex()) {
-			if (!self::$warned) {
-				trigger_error ("Warning: norex() circumventing non-existent permission $group $class $key (limit of one such warning given)");
-				self::$warned = true;
-			}
-			return true;
-		} else return false;
+		return !!self::getAll('where group_id=? and class=? and `key`=? and status=1', 'iss',
+							  $group, $class, $key);
 	}
 
 	function quickformPrefix() {return 'permissions_';}
