361f338ee136faa883d5b6907fa34115043f396f
Adam <adam@Thurloat.local>
2009-03-23 15:14:15 -0300

    Adding notes and instructions to the analytics module.

--- modules/Analytics/Analytics.php
+++ modules/Analytics/Analytics.php
@@ -28,6 +28,20 @@ class Module_Analytics extends Module {
 					'Code' => 'content',
 					'Last Updated' => 'timestamp',
 			 ))
+			 ->Pre('<div>
+			 <p>To view Analytics for your site, visit <a href="http://google.com/analytics" target="new">The Google Analytics </a> page. </p>
+			 <h4>Initial Setup:</h4>
+			 <ul>
+			 	<li> Sign Up for a Google Account, if you don`t have one already | <a href="http://google.com/" target="new">here.</a>
+			 	<li> Step 1 - General Information | <a href="http://img.skitch.com/20090323-kkpygktagf5jauuk2sx187yke3.jpg" rel="facebox">pic</a> </li>
+			 	<li> Step 3 - Agree to the Terms of Service </li>
+			 	<li> Step 4 - Copy the google tracking code  | <a href="http://img.skitch.com/20090323-eexf4ypd139tu5p41eyki6h79m.png" rel="facebox">pic</a></li>
+			 	<li> Step 5 - Create the object in the CMS | <a href="http://img.skitch.com/20090323-r6pie5bmx59bjy4g6pn7k1j9r5.png" rel="facebox">pic</a></li>
+			 	<li> Step 6 - Paste the code and save | <a href=http://img.skitch.com/20090323-r3375fj2iruh1ineja468rp9yj.png" rel="facebox">pic</a></li>
+			 	<li> Step 7 - Profit. </li>
+			 </ul>
+			 </div>')
+			 ->Post('<p>At any time, you can edit the code and it will update site-wide.</p>')
 			 ->name('Analytics Script')
 			 ->showCreate(false);
 			 
