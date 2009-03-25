556f46504da8c6379e64a54fee6df0f855e23812
Adam <adam@Thurloat.local>
2009-03-25 11:28:21 -0300

    Updated tutorial Steps

--- modules/Analytics/Analytics.php
+++ modules/Analytics/Analytics.php
@@ -35,11 +35,11 @@ class Module_Analytics extends Module {
 			 <ul>
 			 	<li> Sign Up for a Google Account, if you don`t have one already | <a href="http://google.com/" target="new">here.</a>
 			 	<li> Step 1 - General Information | <a href="http://img.skitch.com/20090323-kkpygktagf5jauuk2sx187yke3.jpg" rel="facebox">pic</a> </li>
-			 	<li> Step 3 - Agree to the Terms of Service </li>
-			 	<li> Step 4 - Copy the google tracking code  | <a href="http://img.skitch.com/20090323-eexf4ypd139tu5p41eyki6h79m.png" rel="facebox">pic</a></li>
-			 	<li> Step 5 - Create the object in the CMS | <a href="http://img.skitch.com/20090323-r6pie5bmx59bjy4g6pn7k1j9r5.png" rel="facebox">pic</a></li>
-			 	<li> Step 6 - Paste the code and save | <a href=http://img.skitch.com/20090323-r3375fj2iruh1ineja468rp9yj.png" rel="facebox">pic</a></li>
-			 	<li> Step 7 - Profit. </li>
+			 	<li> Step 2 - Agree to the Terms of Service </li>
+			 	<li> Step 3 - Copy the google tracking code  | <a href="http://img.skitch.com/20090323-eexf4ypd139tu5p41eyki6h79m.png" rel="facebox">pic</a></li>
+			 	<li> Step 4 - Create the object in the CMS | <a href="http://img.skitch.com/20090323-r6pie5bmx59bjy4g6pn7k1j9r5.png" rel="facebox">pic</a></li>
+			 	<li> Step 5 - Paste the code and save | <a href=http://img.skitch.com/20090323-r3375fj2iruh1ineja468rp9yj.png" rel="facebox">pic</a></li>
+			 	<li> Step 6 - Profit. </li>
 			 </ul>
 			 </div>')
 			 ->Post('<p>At any time, you can edit the code and it will update site-wide.</p>')
