430b64b8104bec98732fcd5d457326caf717ed3b
Christopher Troup <chris@norex.ca>
2009-03-26 13:24:04 -0400

    add licence header to top of files that we own/wrote.

--- core/Image.php
+++ core/Image.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ *  This file is part of Dashboard.
+ *
+ *  Dashboard is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Dashboard is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Dashboard.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  @license http://www.gnu.org/licenses/gpl.txt
+ *  @copyright Copyright 2007-2009 Norex Core Web Development
+ *  @author See CREDITS file
+ *
+ */
 
 require_once('Database.php');
 class Image {
--- core/plugins/function.amp.php
+++ core/plugins/function.amp.php
@@ -1,17 +1,26 @@
 <?php
-
 /**
- * Smarty plugin to build the javascript for an AJAX call.
- * @file function.helpitem.php
- * @package Smarty
- * @subpackage plugins
+ *  This file is part of Dashboard.
+ *
+ *  Dashboard is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Dashboard is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Dashboard.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  @license http://www.gnu.org/licenses/gpl.txt
+ *  @copyright Copyright 2007-2009 Norex Core Web Development
+ *  @author See CREDITS file
+ *
  */
 
-/* This is used in place of &nbsp; so as to cover up a bug in our templater editor.
- * 
- * @package Smarty
- * @subpackage plugins
- */
 function smarty_function_amp($params) {
 	$arg = array_values($params);
 	$arg = $arg[0];
