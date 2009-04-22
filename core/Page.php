15f3d9bc2f0bb862ccf29729eceadf03c098f6be
Christopher Troup <chris@norex.ca>
2009-04-22 17:03:04 -0400

    add inflector class, make Page class use it

--- core/Page.php
+++ core/Page.php
@@ -202,16 +202,6 @@ class Page extends defaultPageActs {
 		return $this->pointer;
 	}
 	
-	public function getPluralName() {
-		if(substr($this->getName(), 0, -1) == 's') { 
-			return $this->getName() . 'es';
-		} else if(substr($this->getName(), -2) == 'ry') { 
-			return substr($this->getName(), 0, -1) . 'ies';
-		} else {
-			return $this->getName() . 's';
-		}
-	}
-	
 	public function restricted($action, $item){
 		if(key_exists('restrict', $this->pageActions[$this->pointer][$action])){
 			foreach($this->tables[$this->pointer] as $column){
@@ -448,7 +438,7 @@ class Page extends defaultPageActs {
 				
 			
 			if (count($items) == 0) {
-				$html .= '<p>No ' . $this->getPluralName() . ' Created.';
+				$html .= '<p>No ' . Inflector::pluralize($this->getName()) . ' Created.';
 				if ($this->user->hasPerm($this->pointer, $this->pageActions[$this->pointer]['add']['perm'])) {
 					$html .= ' Would you like to <a href="/admin/' . $_REQUEST['module'] . '&amp;section=' . $this->pointer . '&amp;action=add' . @$add . '" class="create">make one</a>?</p>';
 				}
