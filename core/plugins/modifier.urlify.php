5ca6587de8171f6710cb11e9f1d8fb8ab16a89a9
Christopher Troup <chris@norex.ca>
2009-04-22 17:26:46 -0400

    add url slugging and humanizing

--- core/plugins/modifier.urlify.php
+++ core/plugins/modifier.urlify.php
@@ -22,13 +22,5 @@
  */
 
 function smarty_modifier_urlify($str) {
-	$str = htmlentities($str);
-	$str = preg_replace("/(&)([a-z])([a-z]+;)/i", '$2', $str);
-	$str = preg_replace("/[^A-Z0-9]/i", ' ', $str);
-	$str = preg_replace("/\s+/i", ' ', $str);	
-	$str = trim($str);
-	
-	$str = str_replace(' ', '-', $str);
-	
-	return strtolower($str);
+	return Inflector::slug($str);
 }
